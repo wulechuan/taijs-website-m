6d31294eabfb2b40df2cb72dd5c8c0516e735c11
wulechuan <wulechuan@live.com>
2016-07-12 18:35:49 +0800

    rubbish

---
+++ app-dev/scripts/base/base.js
@@ -0,0 +1,571 @@
+(function () {
+	function processParametersPassedIn() {
+		var qString = location.href.match(/\?.*/);
+		if (qString) qString = qString[0].slice(1);
+
+		var qKVPairs = [];
+		if (qString) {
+			qKVPairs = qString.split('&');
+		}
+
+		var psn1; // page sidebar nav Level 1 current
+		var psn2; // page sidebar nav Level 2 current
+		var tabLabel; // id of tabLabel to show if any
+		var bank;
+		var bankHTML;
+
+		if (typeof window.psn === 'object') {
+			if (window.psn.level1) psn1 = window.psn.level1;
+			if (window.psn.level2) psn2 = window.psn.level2;
+		}
+
+		for (var i in qKVPairs){
+			var kvpString = qKVPairs[i];
+			var kvp = kvpString.split('=');
+
+			if (kvp[0] === 'psn1') psn1 = kvp[1];
+			if (kvp[0] === 'psn2') psn2 = kvp[1];
+			if (kvp[0] === 'tabLabel') tabLabel = kvp[1];
+			if (kvp[0] === 'bank') bank = kvp[1];
+			if (kvp[0] === 'bankHTML') bankHTML = kvp[1];
+		}
+
+		return {
+			bank: decodeURIComponent(bank),
+			bankHTML: decodeURIComponent(bankHTML).replace(/\+/g, ' '),
+			tabLabel: tabLabel,
+			psn: {
+				level1: psn1,
+				level2: psn2
+			}
+		};
+	}
+
+
+	var urlParameters = processParametersPassedIn();
+
+
+
+	$('input[placeholder]').each(function () {
+		function _updateInputStyleForGroomingPlaceholder(field) {
+			if (!field) {
+				return false;
+			}
+
+			var tagNameLC = field.tagName.toLowerCase();
+			if (tagNameLC !== 'input' && tagNameLC !== 'textarea') {
+				return false;
+			}
+
+			var classNameToDealWith = 'empty-field';
+			if (field.value) {
+				$(field).removeClass(classNameToDealWith);
+			} else {
+				$(field).addClass(classNameToDealWith);
+			}
+		}
+
+		_updateInputStyleForGroomingPlaceholder(this);
+
+		$(this).on('input', function () {
+			_updateInputStyleForGroomingPlaceholder(this);
+		});
+	});
+
+
+	$('.tab-panel-set').each(function () {
+		var $tabList = $(this).find('.tab-list');
+
+		var $allTabs = $tabList.find('> li');
+		var currentTab = null;
+		var currentItemHint = $tabList.find('> .current-item-hint')[0];
+
+		$allTabs.each(function (index, tab) {
+			var panelId = tab.getAttribute('aria-controls');
+			var panel = $('#'+panelId)[0];
+
+			if (!panel) throw('Can not find controlled panel for tab [expected panel id="'+panelId+'"].');
+
+			panel.elements = { tab: tab };
+			tab.elements = { panel: panel };
+
+		});
+
+		if ($allTabs.length > 1) {
+			$allTabs.on('click', function () {
+				_showPanelAccordingToTab(this);
+			});
+			$allTabs.on('mouseover', function () {
+				_slideHintToTab(this);
+			});
+			$tabList.on('mouseout', function () {
+				_slideHintToTab(currentTab);
+			});
+		}
+
+		var tabToShowAtBegining = $('#panel-tab-'+urlParameters.tabLabel).parent()[0] || $allTabs[0];
+		_showPanelAccordingToTab(tabToShowAtBegining);
+
+		function _slideHintToTab(theTab) {
+			if (!currentItemHint) return false;
+
+			var currentItemHintCssLeft = -20;
+
+			if (!theTab) {
+				currentItemHint.style.clip = '';
+				return true;
+			}
+
+			var _P = $(theTab).offsetParent();
+			var _L = $(theTab).offset().left;
+			var _LP = $(_P).offset().left;
+
+			_L -= _LP;
+			_L -= currentItemHintCssLeft;
+
+			var _W = $(theTab).outerWidth();
+
+			var _R = _L+_W;
+
+
+			currentItemHint.style.clip = 'rect(0px, '+
+				_R+'px, 2px, '+
+				_L+'px)'
+			;
+
+			return true;
+		}
+
+		function _showPanelAccordingToTab(theTab) {
+			currentTab = theTab;
+			_slideHintToTab(theTab);
+
+			for (var i = 0; i < $allTabs.length; i++) {
+				var tab = $allTabs[i];
+				_processOnePairOfTabPanel(tab, (theTab && tab === theTab));
+			}
+		}
+
+		function _processOnePairOfTabPanel(tab, isToShownMyPanel) {
+			if (!tab) return false;
+
+			var panel = tab.elements.panel;
+			if (!panel) return false;
+
+			if (isToShownMyPanel) {
+				panel.setAttribute('aria-hidden', false);
+				$(tab).addClass('current');
+				$(panel).addClass('current');
+				panel.style.display = 'block';
+			} else {
+				panel.setAttribute('aria-hidden', true);
+				$(tab).removeClass('current');
+				$(panel).removeClass('current');
+				panel.style.display = 'none';
+			}
+
+			return true;
+		}
+	});
+
+
+
+	$('dl.initially-collapsed').each(function () {
+		var $allDTs = $(this).find('> dt');
+		var $allDDs = $(this).find('> dd');
+
+		$allDTs.each(function (index, dt) {
+			var dd = $(dt).find('+ dd')[0];
+			if (!dd) throw('Can not find <dd> right after a <dt> element.');
+
+			var ddContent = $(dd).find('> .content')[0];
+			if (!ddContent) throw('Can not find .content within a <dd> element.');
+
+			dd.elements = { dt: dt, content: ddContent };
+			dt.elements = { dd: dd };
+
+			$(dd).removeClass('expanded');
+		});
+
+
+		$allDTs.on('click', function () {
+			_showDDAccordingToDT(this);
+		});
+
+
+		_showDDAccordingToDT(null);
+
+
+
+		function _showDDAccordingToDT(dt) {
+			var theDD = null;
+			if (dt) theDD = dt.elements.dd;
+
+			for (var i = 0; i < $allDDs.length; i++) {
+				var dd = $allDDs[i];
+				if (theDD && dd === theDD) {
+					_processOnePairOfDTDD(dd, 'toggle');
+				} else {
+					_processOnePairOfDTDD(dd, 'collapse');
+				}
+			}
+		}
+
+		function _processOnePairOfDTDD(dd, action) {
+			if (!dd) return false;
+
+			var dt = dd.elements.dt;
+			var content = dd.elements.content;
+
+			if (!dt) return false;
+			if (!content) return false;
+
+			var $dt = $(dt);
+			var $dd = $(dd);
+			var $content = $(content);
+
+			var ddNewHeight;
+
+			var wasCollapsed = !$dd.hasClass('expanded');
+			var needAction = (!wasCollapsed && action==='collapse') || (action==='toggle');
+			if (!needAction) {
+				return true;
+			}
+
+
+			if (wasCollapsed) {
+				if (content.knownHeight > 20) {
+					setTimeout(function () {
+						var ddContentCurrentHeight = $content.outerHeight();
+						if (ddContentCurrentHeight !== content.knownHeight) {
+							content.knownHeight = ddContentCurrentHeight;
+							ddNewHeight = ddContentCurrentHeight;
+							dd.style.height = content.ddNewHeight+'px';
+						}
+					}, 100);
+				} else {
+					content.knownHeight = $content.outerHeight();
+				}
+
+				ddNewHeight = content.knownHeight;
+				dd.style.height = ddNewHeight+'px';
+
+				$dd.addClass('expanded');
+				dd.setAttribute('aria-expanded', true);
+				dd.setAttribute('aria-hidden',   false);
+				$dt.addClass('expanded');
+			} else {
+				dd.style.height = '';
+
+				$dd.removeClass('expanded');
+				dd.setAttribute('aria-expanded', false);
+				dd.setAttribute('aria-hidden',   true);
+				$dt.removeClass('expanded');
+			}
+
+
+			return 0;
+		}
+	});
+
+
+	setPageSidebarNavCurrentItem(urlParameters.psn);
+
+	function updatePageSidebarNavSubMenuForMenuItem(menuItem, action) {
+		var $subMenu = $(menuItem).find('> .menu');
+		var subMenuWasExpanded = $(menuItem).hasClass('coupled-shown');
+		var needAction =
+			(!subMenuWasExpanded && action==='expand') ||
+			(subMenuWasExpanded && action==='collapse') ||
+			(action==='toggle')
+		;
+		if (!needAction) {
+			return 0;
+		}
+
+		if (subMenuWasExpanded) {
+			$(menuItem).removeClass('coupled-shown');
+			$subMenu.slideUp(null);
+		} else {
+			$(menuItem).addClass('coupled-shown');
+			$subMenu.slideDown(null);
+		}
+	}
+
+	function setPageSidebarNavCurrentItem(conf) {
+		conf = conf || {};
+		conf.level1IdPrefix = 'menu-psn-1-';
+		setMenuCurrentItemForLevel(1, 2, $('#page-sidebar-nav'), conf);
+	}
+
+	function setMenuCurrentItemForLevel(level, depth, parentDom, conf) {
+		level = parseInt(level);
+		depth = parseInt(depth);
+		if (!(level > 0) || !(depth >= level)) {
+			throw('Invalid menu level/depth for configuring a menu tree.');
+		}
+		if (typeof conf !== 'object') {
+			throw('Invalid configuration object for configuring a menu tree.');
+		}
+
+		var prefix = conf['level'+level+'IdPrefix'];
+		var desiredId = prefix + conf['level'+level];
+
+		var $allItems = $(parentDom).find('.menu.level-'+level+' > .menu-item');
+		var currentItem;
+		var currentItemId;
+
+		$allItems.each(function (index, menuItem) {
+			var itemLabel = $(menuItem).find('> a > .label')[0];
+			var itemId = itemLabel.id;
+
+			var isCurrentItemOrParentOfCurrentItem = itemId && desiredId && (itemId===desiredId);
+			var isCurrentItem = isCurrentItemOrParentOfCurrentItem && level === depth;
+			if (isCurrentItemOrParentOfCurrentItem) {
+				currentItem = menuItem;
+				currentItemId = itemId;
+				if (isCurrentItem) {
+					$(menuItem).addClass('current');
+					$(menuItem).removeClass('current-parent');
+				} else {
+					$(menuItem).addClass('current-parent');
+					$(menuItem).removeClass('current');
+				}
+			} else {
+				$(menuItem).removeClass('current');
+				$(menuItem).removeClass('current-parent');
+			}
+		});
+
+		var currentSubMenuItem = null;
+		if (level < depth && currentItem) {
+			var nextLevel = level + 1;
+			conf['level'+nextLevel+'IdPrefix'] = currentItemId + '-' + nextLevel + '-';
+			currentSubMenuItem = setMenuCurrentItemForLevel(nextLevel, depth, currentItem, conf);
+			if (currentSubMenuItem) {
+				$(currentItem).addClass('has-sub-menu'); // update this for robustness
+				$(currentItem).addClass('coupled-shown');
+			}
+		}
+
+		return currentSubMenuItem || currentItem;
+	}
+
+	$('.menu-item.has-sub-menu').each(function () {
+		var menuItem = this;
+		var $subMenuHint = $(this).find('> a > .sub-menu-hint, > .sub-menu-hint');
+
+		$subMenuHint.on('click', function (event) {
+			if (event) {
+				event.preventDefault();
+				event.stopPropagation();
+			}
+
+			updatePageSidebarNavSubMenuForMenuItem(menuItem, 'toggle');
+		});
+	});
+
+
+	var $allTabularLists = $('.tabular .f-list');
+
+	$allTabularLists.each(function () {
+		var $allListItems  = $(this).find(' > li.selectable');
+		var $allCheckboxes = $allListItems.find('input[type="checkbox"].selectable-list-item-selector');
+		var $allRadios     = $allListItems.find('input[type="radio"].selectable-list-item-selector');
+		// console.log('has checkboxes: ', $allCheckboxes.length > 0, '\nhas radios: ', $allRadios.length > 0);
+
+		function _updateListItemAccordingToCheckboxStatus(listItem, checkbox) {
+			var $li = $(listItem);
+			if (checkbox.disabled) {
+				$li.addClass('disabled');
+			} else {
+				$li.removeClass('disabled');
+			}
+
+			if (checkbox.checked) {
+				$li.addClass('selected');
+			} else {
+				$li.removeClass('selected');
+			}
+		}
+
+		if ($allCheckboxes.length > 0) {
+			$allListItems.each(function () {
+				var listItem = this;
+				var $listItem = $(this);
+
+
+				var $myCheckbox = $listItem.find('input[type="checkbox"].selectable-list-item-selector');
+				var myCheckbox = $myCheckbox[0];
+
+
+				var _myCheckboxUntouchedYet = true;
+				setTimeout(function () { /* Initializing selection status; And this must dealy because ie8 to ie11 updates cached "checked" statuses very late */
+					if (_myCheckboxUntouchedYet) {
+						_updateListItemAccordingToCheckboxStatus(listItem, myCheckbox);
+					}
+				}, 100);
+
+				if (myCheckbox) {
+					$myCheckbox.on('click', function(event) {
+						if (this.disabled) return false;
+						_myCheckboxUntouchedYet = false;
+						if (event) event.stopPropagation();
+					});
+
+					$listItem.on('click', function () {
+						if (myCheckbox.disabled) return false;
+						myCheckbox.checked = !myCheckbox.checked;
+						_myCheckboxUntouchedYet = false;
+						_updateListItemAccordingToCheckboxStatus(this, myCheckbox);
+					});
+
+					$myCheckbox.on('change', function() {
+						_updateListItemAccordingToCheckboxStatus(listItem, this);
+					});
+				}
+			});
+		}
+
+
+
+
+
+
+
+		function _updateAllListItemsAccordingToRadioStatuses() {
+			for (var i = 0; i < $allListItems.length; i++) {
+				var _li = $allListItems[i];
+				var _radio = _li.elements && _li.elements.radio;
+
+				var $li = $(_li);
+
+				if (_radio.disabled) {
+					$li.addClass('disabled');
+				}
+
+				if (_radio.checked) {
+					$li.addClass('selected');
+				} else {
+					$li.removeClass('selected');
+				}
+			}
+		}
+
+		if ($allRadios.length > 0) {
+			var _radioUntouchedYet = true;
+			setTimeout(function () { /* Initializing selection status; And this must dealy because ie8 to ie11 updates cached "checked" statuses very late */
+				if (_radioUntouchedYet) {
+					_updateAllListItemsAccordingToRadioStatuses();
+				}
+			}, 100);
+
+			$allListItems.each(function () {
+				var listItem = this;
+				var $listItem = $(this);
+
+				var $myRadio = $listItem.find('input[type="radio"].selectable-list-item-selector');
+				var myRadio = $myRadio[0];
+				if (myRadio) {
+					if (typeof listItem.elements !== 'object') listItem.elements = {};
+					listItem.elements.radio = myRadio;
+
+					$listItem.on('click', function () {
+						if (!myRadio.disabled) {
+							_radioUntouchedYet = false;
+							myRadio.checked = true;
+						}
+						_updateAllListItemsAccordingToRadioStatuses();
+					});
+				}
+			});
+		}
+	});
+
+
+
+	$('.drop-down-list').each(function () {
+		var dropDownList = this;
+		var $currentValueContainer = $(this).find('.drop-down-list-current-value');
+		var inputForStoringValue = $(this).find('input.drop-down-list-value');
+		var inputForStoringHTML = $(this).find('input.drop-down-list-value-html');
+		var $options = $(this).find('.drop-down-list-options > li'); // assuming there is only one level of menu
+
+		if (urlParameters.bank && urlParameters.bank !== 'undefined') {
+			_chooseOption(urlParameters.bank, urlParameters.bankHTML);
+		} else {
+			if ($options.length > 0) {
+				wlc.UI.bodyClickListener.register(this, onClickOutside);
+				_chooseOption(0);
+			} else {
+				_chooseOption(null);
+			}
+		}
+
+		$currentValueContainer.on('click', function () {
+			$(dropDownList).toggleClass('coupled-shown');
+		});
+
+		$options.on('click', function () {
+			_chooseOption(this);
+			$(dropDownList).removeClass('coupled-shown');
+		});
+
+		function _chooseOption(chosenOption, chosenOptionHTML) {
+			if (typeof chosenOption === 'number') {
+				chosenOption = $options[chosenOption];
+			}
+
+			var chosenValue;
+
+			if (chosenOption && typeof chosenOption === 'string' && chosenOptionHTML && typeof chosenOptionHTML === 'string') {
+				$currentValueContainer.html(chosenOptionHTML);
+				$(inputForStoringValue).val(chosenOption);
+				$(inputForStoringHTML).val(chosenOptionHTML);
+			}
+
+			if (!chosenOption) {
+				$currentValueContainer[0].innerHTML = '';
+				$(inputForStoringValue).val('');
+				$(inputForStoringHTML).val('');
+				return true;
+			}
+
+			if (!chosenValue) chosenValue = $(chosenOption).find('.value')[0];
+			if (chosenValue) chosenValue = chosenValue.getAttribute('data-value');
+
+			chosenOptionHTML = $(chosenOption).html();
+			$currentValueContainer.html(chosenOptionHTML);
+			$(inputForStoringValue).val(chosenValue);
+			$(inputForStoringHTML).val(chosenOptionHTML);
+		}
+
+		function onClickOutside() {
+			$(dropDownList).removeClass('coupled-shown');
+		}
+	});
+})();
+
+
+(function fakeLogics() {
+	var DC = new webLogicControls.UI.DraggingController(
+		document.body,
+		{
+			movingElement: $('.app-fg-layer')[0],
+			triggerDirection: 'upwards',
+			onFirstTrigger: function (event, options) {
+				console.log('first:', options.status.triggerCount);
+			},
+			onEachTrigger: function(event, options) {
+				console.log('each:', options.status.triggerCount);
+			}
+		}
+	);
+
+	setTimeout(function () {
+		DC.config({
+			triggerDirection: 'downwards',
+			movingElement: $('.row')[0]
+		});
+	}, 5000);
+})();
---
+++ app-dev/scripts/base/wlc.js
@@ -0,0 +1,421 @@
+window.webLogicControls = {};
+
+(function () {
+	var wlc = this;
+
+
+	this.Class = {};
+	(function () { // Class
+
+	}).call(this.Class);
+
+
+	this.DOM = {};
+	(function () { // DOM
+		this.ANestedInB = function (A, B) {
+			if (!(A instanceof Node && B instanceof Node)) return false;
+
+			A = A.parentNode;
+			while (A.tagName && A!==document.body && A!==B) {
+				A = A.parentNode;
+			}
+
+			return A===B;
+		};
+	}).call(this.DOM);
+
+
+	this.UI = {};
+	(function () { // UI
+		this.bodyClickListener = new BodyClickListener();
+		function BodyClickListener() {
+			this.registeredElements = [];
+
+			this.register = function (elements, callback) {
+				if (typeof callback !== 'function') return false;
+
+				if (!Array.isArray(elements)) elements = [elements];
+				for (var i = 0; i < elements.length; i++) {
+					var el = elements[i];
+					if (!el) continue;
+					this.registeredElements.push({
+						element: el,
+						callback: callback
+					});
+				}
+			};
+
+			this.broadCastOutsideClickToRegisteredElements = function (clickedEl) {
+				for (var i = 0; i < this.registeredElements.length; i++) {
+					var record = this.registeredElements[i];
+					var el = record.element;
+					var isOutside = this.testClickOutsideElement(el, clickedEl);
+					if (isOutside) {
+						record.callback(clickedEl);
+					}
+				}
+			};
+
+			this.testClickOutsideElement = function (testEl, clickedEl) {
+				if (!testEl || !clickedEl) return true;
+
+				while (clickedEl && clickedEl!==document.body && clickedEl!==testEl) {
+					clickedEl = clickedEl.parentNode;
+				}
+
+				return testEl !== clickedEl;
+			};
+
+			function _init() {
+				$('body').on('click', (function (event) {
+					var clickedEl = event.target;
+					this.broadCastOutsideClickToRegisteredElements(clickedEl);
+				}).bind(this));
+			}
+
+			_init.call(this);
+		}
+
+
+		this.Menu_NOT_DONE_YET = function (rootElement, initOptions) {
+			// function example() {
+			// 	conf = conf || {};
+			// 	conf.level1IdPrefix = 'menu-chief-1-';
+			// 	setMenuCurrentItemForLevel(1, 2, $('#app-chief-nav'), conf);
+			// }
+
+			this.options = {
+				cssClassItemActive: 'current',
+				cssClassItemParentOfActive: 'current-parent'
+			}
+
+			this.onItemActivate = undefined;
+			this.onItemDeactivate = undefined;
+
+			function setMenuCurrentItemForLevel(level, depth, parentDom, conf) {
+				level = parseInt(level);
+				depth = parseInt(depth);
+				if (!(level > 0) || !(depth >= level)) {
+					throw('Invalid menu level/depth for configuring a menu tree.');
+				}
+				if (typeof conf !== 'object') {
+					throw('Invalid configuration object for configuring a menu tree.');
+				}
+
+				var prefix = conf['level'+level+'IdPrefix'];
+				var desiredId = prefix + conf['level'+level];
+
+				var $allItems = $(parentDom).find('.menu.level-'+level+' > .menu-item');
+				var currentItem;
+				var currentItemId;
+
+				$allItems.each(function (index, menuItem) {
+					var itemLabel = $(menuItem).find('> a > .label')[0];
+					var itemId = itemLabel.id;
+
+					var isCurrentItemOrParentOfCurrentItem = itemId && desiredId && (itemId===desiredId);
+					var isCurrentItem = isCurrentItemOrParentOfCurrentItem && level === depth;
+					if (isCurrentItemOrParentOfCurrentItem) {
+						currentItem = menuItem;
+						currentItemId = itemId;
+						if (isCurrentItem) {
+							$(menuItem).addClass('current');
+							$(menuItem).removeClass('current-parent');
+						} else {
+							$(menuItem).addClass('current-parent');
+							$(menuItem).removeClass('current');
+						}
+					} else {
+						$(menuItem).removeClass('current');
+						$(menuItem).removeClass('current-parent');
+					}
+				});
+
+				var currentSubMenuItem = null;
+				if (level < depth && currentItem) {
+					var nextLevel = level + 1;
+					conf['level'+nextLevel+'IdPrefix'] = currentItemId + '-' + nextLevel + '-';
+					currentSubMenuItem = setMenuCurrentItemForLevel(nextLevel, depth, currentItem, conf);
+					if (currentSubMenuItem) {
+						$(currentItem).addClass('has-sub-menu'); // update this for robustness
+						$(currentItem).addClass('coupled-shown');
+					}
+				}
+
+				return currentSubMenuItem || currentItem;
+			}
+		}
+
+
+		this.DraggingController = function(rootElement, initOptions) {
+			if (!(rootElement instanceof Node)) {
+				throw('Invalid rootElement for constructing a '+this.constructor.name+'.');
+			}
+
+			this.options = {
+				maxOffsetX: 120,
+				maxOffsetY: 180,
+				triggerX: 60,
+				triggerY: 90,
+				triggerDirection: 'downwards'
+			};
+
+			this.onFirstTrigger = undefined;
+			this.onEachTrigger = undefined;
+
+			this.config = function (options) {
+				config.call(this, options);
+			};
+
+			var status = {
+				triggerCount: 0,
+				hasTriggeredAtLeastOnce: false,
+				justTriggered: false,
+				mouseDownEvent: null,
+				draggingDirectionIsHorizontal: undefined,
+				draggingDirectionIsNegative: undefined,
+				isDraggingAlongTriggerDirection: false,
+				draggingDirectionHasBeenDecided: false
+			};
+
+			var data = {
+				movingElementOldInlineTransform: ''
+			};
+
+			var triggerCallBackOptions = {
+				rootElement: rootElement,
+				movingElement: rootElement,
+				status: status
+			};
+
+			function onMouseDown(event) {
+				prepareDraggingOnMouseDown.call(this, event);
+			}
+
+			function onMouseUp() {
+				clearStatus();
+				triggerCallBackOptions.movingElement.style.transform = data.movingElementOriginalInlineTransform;
+			}
+
+			function onMouseMove (event) {
+				if (status.shouldCancelDragging) {
+					clearStatus();
+				} else {
+					tryToTriggerOnMouseMove.call(this, event);
+				}
+			}
+
+
+
+
+			function config(options) {
+				options = options || {};
+
+				if (options.hasOwnProperty('movingElement')) {
+					var _r = rootElement;
+					var me = options.movingElement;
+					if (me instanceof Node) {
+						if (wlc.DOM.ANestedInB(_r, me)) {
+							console.warn('DraggingController: The rootElement is a descendant of the movingElement.');
+						}
+					} else if (me === null) {
+						me = _r;
+					} else {
+						me = undefined;
+					}
+
+					if (me) {
+						var cbo = triggerCallBackOptions;
+
+						if (cbo.movingElement instanceof Node) {
+							cbo.movingElement.style.transform = data.movingElementOriginalInlineTransform;
+						}
+
+						cbo.movingElement = me;
+						data.movingElementOriginalInlineTransform = me.style.transform;
+
+						if (status.mouseDownEvent) status.shouldCancelDragging = true;
+					}
+				}
+
+				switch (options.triggerDirection) {
+					case 'leftwards':
+					case 'rightwards':
+					case 'upwards':
+					case 'downwards':
+						this.options.triggerDirection = options.triggerDirection;
+						break;
+					default:
+				}
+
+				var _O = this.options;
+
+				if (options.maxOffsetX > 0) _O.maxOffsetX = options.maxOffsetX;
+				if (options.maxOffsetY > 0) _O.maxOffsetY = options.maxOffsetY;
+
+				if (options.triggerX > 0) _O.triggerX = Math.min(_O.maxOffsetX, options.triggerX);
+				if (options.triggerY > 0) _O.triggerY = Math.min(_O.maxOffsetY, options.triggerY);
+
+				_O.triggerResetX = _O.triggerX * 0.75;
+				_O.triggerResetY = _O.triggerY * 0.75;
+
+				if (options.triggerResetX > 0) _O.triggerResetX = Math.min(_O.triggerResetX, options.triggerResetX);
+				if (options.triggerResetY > 0) _O.triggerResetY = Math.min(_O.triggerResetY, options.triggerResetY);
+
+				if (typeof options.onFirstTrigger === 'function') this.onFirstTrigger = options.onFirstTrigger;
+				if (typeof options.onEachTrigger === 'function') this.onEachTrigger = options.onEachTrigger;
+			}
+
+			function prepareDraggingOnMouseDown(event) {
+				status.mouseDownEvent = event;
+				switch (this.options.triggerDirection) {
+					case 'leftwards':
+					case 'rightwards':
+						status.draggingDirectionIsHorizontal = true;
+						break;
+
+					default:
+					case 'upwards':
+					case 'downwards':
+						status.draggingDirectionIsHorizontal = false;
+						break;
+				}
+			}
+
+			function clearStatus() {
+				status.shouldCancelDragging = false;
+				status.triggerCount = 0;
+				status.hasTriggeredAtLeastOnce = false;
+				status.justTriggered = false;
+				status.mouseDownEvent = null;
+				status.draggingDirectionIsHorizontal = undefined;
+				status.draggingDirectionIsNegative = undefined;
+				status.isDraggingAlongTriggerDirection = false;
+				status.draggingDirectionHasBeenDecided = false;
+			}
+
+			function tryToTriggerOnMouseMove (event) {
+				var _E = status.mouseDownEvent;
+				if (!_E) return false;
+
+				var x1 = _E.pageX;
+				var y1 = _E.pageY;
+
+				var x2 = event.pageX;
+				var y2 = event.pageY;
+
+				var dx = x2 - x1;
+				var dy = y2 - y1;
+
+				var dxA = Math.abs(dx);
+				var dyA = Math.abs(dy);
+
+
+				if (!status.draggingDirectionHasBeenDecided) {
+					var hasDraggedFarEnough = dy*dy + dx*dx >= 50;
+					if (hasDraggedFarEnough) {
+						status.draggingDirectionHasBeenDecided = true;
+						switch (this.options.triggerDirection) {
+							case 'leftwards':
+								status.isDraggingAlongTriggerDirection = dx < -5 && dxA > dyA*3;
+								status.draggingDirectionIsNegative = status.isDraggingAlongTriggerDirection && dx < 0;
+								break;
+
+							case 'rightwards':
+								status.isDraggingAlongTriggerDirection = dx >  5 && dxA > dyA*3;
+								status.draggingDirectionIsNegative = status.isDraggingAlongTriggerDirection && dx < 0;
+								break;
+
+							case 'upwards':
+								status.isDraggingAlongTriggerDirection = dy < -5 && dyA > dxA*3;
+								status.draggingDirectionIsNegative = status.isDraggingAlongTriggerDirection && dy < 0;
+								break;
+
+							default:
+							case 'downwards':
+								status.isDraggingAlongTriggerDirection = dy >  5 && dyA > dxA*3;
+								status.draggingDirectionIsNegative = status.isDraggingAlongTriggerDirection && dy < 0;
+								break;
+						}
+					}
+				} else if (status.isDraggingAlongTriggerDirection) {
+					var mayTrigger = false;
+					var mayResetTrigger = false;
+
+					if (status.draggingDirectionIsHorizontal) {
+						var maxX = this.options.maxOffsetX;
+						var targetOffsetX = dx;
+
+						mayTrigger      = dxA >= this.options.triggerX;
+						mayResetTrigger = dxA <= this.options.triggerResetX;
+
+
+						if (dxA >= maxX) {
+							targetOffsetX = status.draggingDirectionIsNegative ? -maxX : maxX;
+						}
+						if (status.draggingDirectionIsNegative) {
+							if (dx > 0) targetOffsetX = 0;
+						} else {
+							if (dx < 0) targetOffsetX = 0;
+						}
+
+						triggerCallBackOptions.movingElement.style.transform = 'translateX('+targetOffsetX+'px)';
+					} else {
+						var maxY = this.options.maxOffsetY;
+						var targetOffsetY = dy;
+
+						mayTrigger      = dyA >= this.options.triggerY;
+						mayResetTrigger = dyA <= this.options.triggerResetY;
+
+
+						if (dyA >= maxY) {
+							targetOffsetY = status.draggingDirectionIsNegative ? -maxY : maxY;
+						}
+						if (status.draggingDirectionIsNegative) {
+							if (dy > 0) targetOffsetY = 0;
+						} else {
+							if (dy < 0) targetOffsetY = 0;
+						}
+
+						triggerCallBackOptions.movingElement.style.transform = 'translateY('+targetOffsetY+'px)';
+					}
+
+					if (mayTrigger) {
+						var _shouldSkipCountInc = false;
+						if (!status.hasTriggeredAtLeastOnce) {
+							status.hasTriggeredAtLeastOnce = true;
+							status.triggerCount = 1;
+							_shouldSkipCountInc = true;
+							if (typeof this.onFirstTrigger === 'function') this.onFirstTrigger(event, triggerCallBackOptions);
+						}
+
+						if (!status.justTriggered) {
+							if (!_shouldSkipCountInc) status.triggerCount++;
+							status.justTriggered = true;
+							if (typeof this.onEachTrigger === 'function') this.onEachTrigger(event, triggerCallBackOptions);
+						}
+					}
+
+					if (status.justTriggered && mayResetTrigger) {
+						status.justTriggered = false;
+					}
+				}
+			}
+
+			function init () {
+				this.config(initOptions);
+
+				var $_r = $(rootElement);
+				$_r
+					.on('mousedown', onMouseDown.bind(this))
+					.on('mouseup',   onMouseUp  .bind(this))
+					.on('mousemove', onMouseMove.bind(this))
+				;
+
+				clearStatus();
+			}
+
+			init.call(this);
+		};
+	}).call(this.UI);
+}).call(window.webLogicControls);
