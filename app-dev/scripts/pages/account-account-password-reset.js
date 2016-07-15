924beda39a9d560c2832e72639579161a5869c71
wulechuan <wulechuan@live.com>
2016-07-15 16:57:42 +0800

    rename pages; excluded useless pages

--- app-dev/scripts/pages/account-account-password-reset.js
+++ app-dev/scripts/pages/account-account-password-reset.js
@@ -2,18 +2,6 @@ $(function () {
 	var $pL1 = $('#pl-info-too-many-vcodes-today');
 	var $pL2 = $('#pl-input-image-vcode');
 
-
-
-
-	$('[button-action="sign-in"]').on('click', function(event) {
-		console.log('sign-in');
-	});
-
-	$('[button-action="sign-up"]').on('click', function(event) {
-		console.log('sign-up');
-	});
-
-
 	var $thePL = $pL1;
 	$('[button-action="fetch-sms-vcode"]').on('click', function(event) {
 		console.error('fake logic triggered.');
