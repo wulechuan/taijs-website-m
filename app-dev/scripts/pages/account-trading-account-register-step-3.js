33874d43af9f5f0a42bec4a298e1bdb83fb9b335
wulechuan <wulechuan@live.com>
2016-07-27 17:11:01 +0800

    input fields tips all done

--- app-dev/scripts/pages/account-trading-account-register-step-3.js
+++ app-dev/scripts/pages/account-trading-account-register-step-3.js
@@ -1,10 +1,21 @@
 $(function () {
 	var wlc = window.webLogicControls;
+
+	var $plpm1 = $('#plpm-trading-account-register-passwords-mismatch');
+	function showPoliteMessagePasswordsMismatch() {
+		$plpm1.show();
+		if ($plpm1.length > 0) {
+			setTimeout(function () {
+				$plpm1.hide();
+			}, 6000);
+		}
+	}
+
 	var $passwordPass1Container = $('.password-pass-1');
 	var $passwordPass2Container = $('.password-pass-2');
 	var passwordInput1 = $('.password-pass-1 .single-char-inputs-set .single-char-inputs-aggregator')[0];
 	var passwordInput2 = $('.password-pass-2 .single-char-inputs-set .single-char-inputs-aggregator')[0];
-	var buttonConfirm = $('[button-action="submit"]')[0];
+	// var buttonConfirm = $('[button-action="submit"]')[0];
 
 	var singleCharInputsSet1 = new wlc.UI.SingleCharacterInputsSet($('.password-pass-1 .single-char-inputs-set')[0], {
 		onAllInputsValid: function (aggregatedValue, isCheckingOnLoad) {
@@ -13,15 +24,17 @@ $(function () {
 			setTimeout(function () {
 				singleCharInputsSet2.focus();
 			}, 0);
+			$plpm1.hide();
 		}
 	});
 
 	var singleCharInputsSet2 = new wlc.UI.SingleCharacterInputsSet($('.password-pass-2 .single-char-inputs-set')[0], {
 		onAllInputsValid: function (aggregatedValue, isCheckingOnLoad) {
 			if ((passwordInput1.value === passwordInput2.value) && passwordInput1.value.length) {
-				if (buttonConfirm) buttonConfirm.disabled = false;
+				// if (buttonConfirm) buttonConfirm.disabled = false;
+				location.assign('account-trading-account-register-succeeded.html');
 			} else {
-				if (buttonConfirm) buttonConfirm.disabled = true;
+				// if (buttonConfirm) buttonConfirm.disabled = true;
 				singleCharInputsSet1.clear();
 				singleCharInputsSet2.clear();
 				$passwordPass1Container.show();
@@ -29,36 +42,11 @@ $(function () {
 				setTimeout(function () {
 					singleCharInputsSet1.focus();
 				}, 0);
-				$('.row.error-tip').show();
+				// $('.row.error-tip').show();
+				showPoliteMessagePasswordsMismatch();
 			}
 		}
 	});
 
 
-});
-
-$(function () { // fake logics
-	var wlc = window.webLogicControls;
-	var $pL1 = $('#pl-password-input-panel-trading');
-
-	new wlc.UI.SingleCharacterInputsSet($pL1.find('.single-char-inputs-set')[0], {
-		onAllInputsValid: function (aggregatedValue, status, isCheckingOnLoad) {
-			console.log('AWESOME! final value:', aggregatedValue);
-			if (isCheckingOnLoad) console.log('What\'s better, we did nothing to get this!');
-		}
-	});
-
-	var $thePL = $pL1;
-	$('.progress-stops').on('click', function(event) {
-		console.error('fake logic triggered.');
-		var $bp = $thePL[0].elements.$popupLayersBackPlate;
-		$bp.show();
-		$thePL.show();
-		$thePL.add($bp).on('click', function (event) {
-			if (event.target === $thePL[0]) {
-				$bp.hide();
-				$thePL.hide();
-			}
-		});
-	});
 });
