296aaec196de032ed081bb151c4e6ea3ef37223b
wulechuan <wulechuan@live.com>
2016-08-03 15:30:33 +0800

    before building tabpanelsetcontroller

---
+++ app-dev/scripts/pages/my-fixed-income-index.js
@@ -0,0 +1,68 @@
+$(function () {
+	(function () {
+		var panelsSwiper = new window.Swiper('.swiper-container', {
+			pagination: '.tab-list',
+			paginationType: 'custome',
+			paginationElement: 'li',
+			paginationClickable: true
+
+		});
+
+		panelsSwiper.on('slideChangeStart', function (swiper) {
+			var indexOfSlideToShow = swiper.activeIndex;
+			
+		});
+	})();
+
+	$('.page').each(function () {
+		function getScrollingOffsetFor(content) {
+			if (!content || content === document) {
+				return {
+					top:  -window.scrollY,
+					left: -window.scrollX
+				};
+			}
+
+			return $(content).offset();
+		}
+		function updatePageHeaderModeBasedOnScrolling() {
+			var shouldSwitchPageHeaderMode = false;
+			var currentTopOffset = getScrollingOffsetFor(scrollingElement).top;
+
+			if (pageHeaderIsInCompactMode) {
+				shouldSwitchPageHeaderMode = currentTopOffset >= currentTopOffsetThresholdForExpansion;
+				if (shouldSwitchPageHeaderMode) {
+					$page.removeClass('page-header-in-compact-mode');
+					pageHeaderIsInCompactMode = false;
+				}
+			} else {
+				shouldSwitchPageHeaderMode = currentTopOffset <= currentTopOffsetThresholdForCollapse;
+				if (shouldSwitchPageHeaderMode) {
+					$page.addClass('page-header-in-compact-mode');
+					pageHeaderIsInCompactMode = true;
+				}
+			}
+		}
+
+
+		var scrollingElement = document;
+		var $page = $(this);
+
+
+		var triggerRatioForCallapse = 1.2;
+		var triggerRatioForExpansion = 0.8;
+		var pageHeaderNormalHeight = 128;
+		var pageHeaderCompactHeight = 48;
+
+
+
+		var pageHeaderHeightDelta = pageHeaderNormalHeight - pageHeaderCompactHeight;
+		var currentTopOffsetThresholdForCollapse  = Math.abs(pageHeaderHeightDelta) * -Math.abs(triggerRatioForCallapse);
+		var currentTopOffsetThresholdForExpansion = Math.abs(pageHeaderHeightDelta) * -Math.abs(triggerRatioForExpansion);
+		var pageHeaderIsInCompactMode = $page.hasClass('page-header-in-compact-mode');
+
+
+		updatePageHeaderModeBasedOnScrolling();
+		$(scrollingElement).on('scroll', updatePageHeaderModeBasedOnScrolling);
+	});
+});
