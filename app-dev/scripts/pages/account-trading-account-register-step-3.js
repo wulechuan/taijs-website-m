cf05bca1d58b2ee8e7758b18eefa99833fc3e0d9
wulechuan <wulechuan@live.com>
2016-08-10 09:32:30 +0800

    step2

--- app-dev/scripts/pages/account-trading-account-register-step-3.js
+++ app-dev/scripts/pages/account-trading-account-register-step-3.js
@@ -2,49 +2,55 @@ $(function () {
 	var wlc = window.webLogicControls;
 	var UI = wlc.UI;
 
-
-	var $plpm1 = $('#plpm-trading-account-register-passwords-mismatch');
-	function showPoliteMessagePasswordsMismatch() {
-		var plpm1 = $plpm1[0];
-		UI.popupLayersManager.show(plpm1);
+	var test = true;
+	if (test) {
+		var $result = $('.test-block .result');
+		$('.test-block .single-char-input').on('input', function (event) {
+			$result.html(this.value);
+		});
 	}
 
-	var $passwordPass1Container = $('.password-pass-1');
-	var $passwordPass2Container = $('.password-pass-2');
-	var passwordInput1 = $('.password-pass-1 .single-char-inputs-set .single-char-inputs-aggregator')[0];
-	var passwordInput2 = $('.password-pass-2 .single-char-inputs-set .single-char-inputs-aggregator')[0];
-	// var buttonConfirm = $('[button-action="submit"]')[0];
 
-	var singleCharInputsSet1 = new wlc.UI.SingleCharacterInputsSet($('.password-pass-1 .single-char-inputs-set')[0], {
-		onAllInputsValid: function (aggregatedValue, isCheckingOnLoad) {
-			$passwordPass1Container.hide();
-			$passwordPass2Container.show();
-			setTimeout(function () {
-				singleCharInputsSet2.focus();
-			}, 0);
-			$plpm1.hide();
-		}
-	});
+	// var $plpm1 = $('#plpm-trading-account-register-passwords-mismatch');
+	// function showPoliteMessagePasswordsMismatch() {
+	// 	var plpm1 = $plpm1[0];
+	// 	UI.popupLayersManager.show(plpm1);
+	// }
 
-	var singleCharInputsSet2 = new wlc.UI.SingleCharacterInputsSet($('.password-pass-2 .single-char-inputs-set')[0], {
-		onAllInputsValid: function (aggregatedValue, isCheckingOnLoad) {
-			if ((passwordInput1.value === passwordInput2.value) && passwordInput1.value.length) {
-				// if (buttonConfirm) buttonConfirm.disabled = false;
-				location.assign('account-trading-account-register-succeeded.html');
-			} else {
-				// if (buttonConfirm) buttonConfirm.disabled = true;
-				singleCharInputsSet1.clear();
-				singleCharInputsSet2.clear();
-				$passwordPass1Container.show();
-				$passwordPass2Container.hide();
-				setTimeout(function () {
-					singleCharInputsSet1.focus();
-				}, 0);
-				// $('.row.error-tip').show();
-				showPoliteMessagePasswordsMismatch();
-			}
-		}
-	});
+	// var $passwordPass1Container = $('.password-pass-1');
+	// var $passwordPass2Container = $('.password-pass-2');
+	// var passwordInput1 = $('.password-pass-1 .single-char-inputs-set .single-char-inputs-aggregator')[0];
+	// var passwordInput2 = $('.password-pass-2 .single-char-inputs-set .single-char-inputs-aggregator')[0];
+	// // var buttonConfirm = $('[button-action="submit"]')[0];
 
+	// var singleCharInputsSet1 = new wlc.UI.SingleCharacterInputsSet($('.password-pass-1 .single-char-inputs-set')[0], {
+	// 	onAllInputsValid: function (aggregatedValue, isCheckingOnLoad) {
+	// 		$passwordPass1Container.hide();
+	// 		$passwordPass2Container.show();
+	// 		setTimeout(function () {
+	// 			singleCharInputsSet2.focus();
+	// 		}, 0);
+	// 		$plpm1.hide();
+	// 	}
+	// });
 
+	// var singleCharInputsSet2 = new wlc.UI.SingleCharacterInputsSet($('.password-pass-2 .single-char-inputs-set')[0], {
+	// 	onAllInputsValid: function (aggregatedValue, isCheckingOnLoad) {
+	// 		if ((passwordInput1.value === passwordInput2.value) && passwordInput1.value.length) {
+	// 			// if (buttonConfirm) buttonConfirm.disabled = false;
+	// 			location.assign('account-trading-account-register-succeeded.html');
+	// 		} else {
+	// 			// if (buttonConfirm) buttonConfirm.disabled = true;
+	// 			singleCharInputsSet1.clear();
+	// 			singleCharInputsSet2.clear();
+	// 			$passwordPass1Container.show();
+	// 			$passwordPass2Container.hide();
+	// 			setTimeout(function () {
+	// 				singleCharInputsSet1.focus();
+	// 			}, 0);
+	// 			// $('.row.error-tip').show();
+	// 			showPoliteMessagePasswordsMismatch();
+	// 		}
+	// 	}
+	// });
 });
