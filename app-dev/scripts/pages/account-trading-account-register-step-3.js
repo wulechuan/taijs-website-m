24e67306cd51f6e8b9cfdaf3efffee5e9c128c65
wulechuan <wulechuan@live.com>
2016-08-10 16:39:39 +0800

    WIP on fixed-count-chars-input-blokc; concept proven

--- app-dev/scripts/pages/account-trading-account-register-step-3.js
+++ app-dev/scripts/pages/account-trading-account-register-step-3.js
@@ -2,6 +2,9 @@ $(function () {
 	var wlc = window.webLogicControls;
 	var UI = wlc.UI;
 
+	function FixedCountCharsInput(rootElement) {
+	}
+
 	var test = true;
 	if (test) {
 		var $result = $('.test-block .result');
@@ -9,26 +12,66 @@ $(function () {
 			$result.html(this.value);
 		});
 
-		var charWidth = detectCorrectCharWidthForPasswordInput(
-			$('.app-bg-layer')[0],
+		detectCorrectCharWidthForPasswordInput(
+			document.body || $('.app-bg-layer')[0],
 			null,
 			5,
+			onResultEvaluated,
 			true
 		);
 
-		updateInputElement($('input.single-char-input-password')[0], null, charWidth, 6);
-		// $(document).on('resize', updateInputElement);
+		var grids = $('.fixed-count-chars-input-block .deco-grid');
+		var $grids = [];
+		for (var i = 0; i < grids.length; i++) {
+			$grids[i] = $(grids[i]);
+		}
+
+		var inputElement = $('input.fixed-count-chars-input')[0];
+		if (inputElement && typeof inputElement.value === 'string') {
+			updateDecoGrids(inputElement.value.length);
+		}
+
+		$(inputElement).on('input', function () {
+			updateDecoGrids(this.value.length);
+		});
 	}
 
-	function updateInputElement(inputElement, widthElement, charWidth, charCount) {
+	function updateDecoGrids(shownCount) {
+		var charCount = Math.min(grids.length, shownCount);
+
+		for (var i = 0; i < grids.length; i++) {
+			if (i < charCount) {
+				$grids[i].addClass('filled');
+			} else {
+				$grids[i].removeClass('filled');
+			}
+		}
+	}
+
+	function onResultEvaluated(charWidth) {
+		updateInputElement(
+			$('.fixed-count-chars-input-block')[0],
+			charWidth,
+			6
+		);
+	}
+
+	function updateInputElement(rootElement, charWidth, charCount) {
 		charCount = parseInt(charCount);
 		if (isNaN(charCount) || charCount < 1) return;
 
 		charWidth = parseFloat(charWidth);
 		if (isNaN(charWidth) || charWidth < 2.19) return;
 
-		if (!inputElement) return;
-		if (!widthElement) widthElement = inputElement;
+		if (!rootElement) return;
+
+		var inputElement = $(rootElement).find('input.fixed-count-chars-input');
+		if (inputElement.length !== 1) return;
+		inputElement = inputElement[0];
+
+		var widthElement = $(rootElement).find('.width-wrapper');
+		if (widthElement.length !== 1) return;
+		widthElement = widthElement[0];
 
 		// inputElement often need to be wider than desired width limitation,
 		// otherwise when the caret moves to the end of the input,
@@ -41,72 +84,119 @@ $(function () {
 		C.l("total width:", moduleWidth, '\t charWidth:', charWidth, '\t charCount:', charCount, '\t letterSpacing:', letterSpacing, '\t indent:', textIndent);
 
 		inputElement.style.letterSpacing = letterSpacing + 'px';
-		inputElement.style.textIndent = textIndent + 'px';
+		inputElement.style.left = textIndent + 'px';
 	}
 
-	function detectCorrectCharWidthForPasswordInput(parentNode, testChar, testSpansCount, shouldTrace) {
-		// var parentNode = $('.app-bg-layer')[0];
-		if (!parentNode) {
-			C.e('Detecting failed.');
-			return NaN;
+	function detectCorrectCharWidthForPasswordInput(parentNode, testChar, testSpansCount, onResultEvaluated, shouldTrace) {
+		var options = {
+			testerCssClassName: 'fixed-count-chars-input-tester'
+		};
+
+		var noError = true;
+		if (typeof onResultEvaluated !== 'function') {
+			C.w('No callback provided. Nothing will happen when char width evaluated.');
 		}
 
-		if (typeof testChar !== 'string' || testChar.length < 1) {
-			testChar = '\u2022';
-		} else {
-			testChar = testChar.charAt(0);
-		}
-
-		testSpansCount = parseInt(testSpansCount);
-		if (isNaN(testSpansCount) || testSpansCount < 3) testSpansCount = 3;
-
+		var wrapperNode;
 		var spans = [];
-		var textContent = testChar;
-		var i;
 
-		var wrapperNode = document.createElement('SPAN');
+		if (!parentNode) {
+			noError = false;
+		} else {
+			_setup();
 
-		for (i = 0; i < testSpansCount; i++) {
-			var span = document.createElement('SPAN');
-			span.innerHTML = textContent;
-			span.className = 'single-char-input-password-tester';
-			span.style.position = 'absolute';
-			wrapperNode.appendChild(span);
-			spans.push(span);
-
-			textContent += testChar;
+			setTimeout(function () {
+				_callCallback(_evaluate());
+			}, 79);
 		}
 
-		parentNode.appendChild(wrapperNode);
+		if (noError) {
+		} else {
+			C.e('Detecting failed.');
+			// _callCallback(0);
+		}
 
-		var spanWidths = [];
-		var charWidths = [];
-		var lastSpanWidth = 0;
-		for (i = 0; i < spans.length; i++) {
-			spanWidths[i] = $(spans[i]).outerWidth();
-			charWidths[i] = spanWidths[i] - lastSpanWidth;
-			lastSpanWidth = spanWidths[i];
-			if (shouldTrace) {
-				C.l('width['+i+']:',charWidths[i], spanWidths[i]);
+
+		return noError;
+
+
+		function _setup() {
+			if (typeof testChar !== 'string' || testChar.length < 1) {
+				testChar = '\u2022';
+			} else {
+				testChar = testChar.charAt(0);
 			}
+
+			testSpansCount = parseInt(testSpansCount);
+			if (isNaN(testSpansCount) || testSpansCount < 3) testSpansCount = 3;
+
+			var innerHTML = testChar;
+			var i;
+
+			wrapperNode = document.createElement('SPAN');
+
+			for (i = 0; i < testSpansCount; i++) {
+				var span = document.createElement('SPAN');
+				span.innerHTML = innerHTML;
+				span.className = options.testerCssClassName;
+				span.style.position = 'absolute';
+				wrapperNode.appendChild(span);
+				spans.push(span);
+
+				innerHTML += testChar;
+			}
+
+			parentNode.appendChild(wrapperNode);
 		}
 
-		var charWidthBiases = [];
-		var maxBias = 0;
-		for (i = 1; i < spans.length; i++) {
-			charWidthBiases[i] = charWidths[i] - charWidths[i-1];
-			maxBias = Math.max(charWidthBiases[i], maxBias);
+		function _evaluate() {
+			var spanWidths = [];
+			var charWidths = [];
+			var lastSpanWidth = 0;
+
+			var log = [];
+			var i;
+			for (i = 0; i < spans.length; i++) {
+				spanWidths[i] = $(spans[i]).outerWidth();
+				charWidths[i] = spanWidths[i] - lastSpanWidth;
+				lastSpanWidth = spanWidths[i];
+				if (shouldTrace) {
+					log.push('width['+i+']: ' + charWidths[i] + ' ' + spanWidths[i]);
+				}
+			}
+
+			var charWidthBiases = [];
+			var maxBias = 0;
+			for (i = 1; i < spans.length; i++) {
+				charWidthBiases[i] = charWidths[i] - charWidths[i-1];
+				maxBias = Math.max(charWidthBiases[i], maxBias);
+			}
+			log.push('maxBias: ' + maxBias);
+
+
+			if (shouldTrace) {
+				var logC = log.join('\n');
+				var logH = log.join('<br>\n');
+
+				C.l(logC);
+
+				var para = document.createElement('P');
+				para.innerHTML = logH;
+				document.body.appendChild(para);
+			}
+
+			var charWidth = charWidths[0];
+			if (maxBias > 2) charWidth = NaN;
+
+			parentNode.removeChild(wrapperNode);
+
+			return charWidth;
 		}
-		if (shouldTrace) {
-			C.l('maxBias:', maxBias);
+
+		function _callCallback(charWidth) {
+			if (typeof onResultEvaluated !== 'function') return;
+			onResultEvaluated(charWidth);
 		}
-
-		var charWidth = charWidths[0];
-		if (maxBias > 2) charWidth = NaN;
-
-		parentNode.removeChild(wrapperNode);
-
-		return charWidth;
 	}
 
 
