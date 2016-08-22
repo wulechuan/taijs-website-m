2adf6540b9109ce6ea49e49e540bdb31269efc55
wulechuan <wulechuan@live.com>
2016-08-22 15:18:40 +0800

    fund status detail page ok

--- app-dev/scripts/pages/my-fund-trading-record-detail.js
+++ app-dev/scripts/pages/my-fund-trading-record-detail.js
@@ -1,7 +1,7 @@
 $(function () {
 	var app = window.taijs.app;
 	var URIP = app.data.URIParameters;
-	C.l(URIP);
+	// C.l(URIP);
 
 	// var wlc = window.webLogicControls;
 	// var UI = wlc.UI;
@@ -10,7 +10,6 @@ $(function () {
 	if (typeof URIProductCaption === 'string') {
 		URIProductCaption = URIProductCaption.replace(/^\s*['"]\s*/, '').replace(/\s*['"]\s*$/, '');
 	}
-	var URITradingStatus = URIP.tradingStatus;
 
 	var progressStopsBlocksId = [
 		'#assest-progress-mai-ru-shen-qing-yi-shou-li',
@@ -22,43 +21,19 @@ $(function () {
 		'#assest-progress-mai-chu-shen-qing-cheng-gong',
 		'#assest-progress-mai-chu-que-ren-cheng-gong',
 		'#assest-progress-mai-chu-que-ren-shi-bai',
-		'#assest-progress-mai-chu-yi-che-dan',
-		'#'
+		'#assest-progress-mai-chu-yi-che-dan'
 	];
 
-	// var stateStrings = {
-	// 	'#assest-progress-mai-ru-shen-qing-yi-shou-li': '买入申请已受理',
-	// 	'#assest-progress-mai-ru-kou-kuan-shi-bai': '买入扣款失败',
-	// 	'#assest-progress-mai-ru-kou-kuan-cheng-gong': '买入扣款成功',
-	// 	'#assest-progress-mai-ru-que-ren-cheng-gong': '买入确认成功',
-	// 	'#assest-progress-mai-ru-que-ren-shi-bai': '买入确认失败',
-	// 	'#assest-progress-mai-ru-yi-che-dan': '买入已撤单',
-	// 	'#assest-progress-mai-chu-shen-qing-cheng-gong': '卖出申请成功',
-	// 	'#assest-progress-mai-chu-que-ren-cheng-gong': '卖出确认成功',
-	// 	'#assest-progress-mai-chu-que-ren-shi-bai': '卖出确认失败',
-	// 	'#assest-progress-mai-chu-yi-che-dan': '卖出已撤单',
-	// };
+	var blockIdToShow = progressStopsBlocksId[Math.floor(Math.random() * progressStopsBlocksId.length)];
 
-	var progressStopsBlocksIdString = progressStopsBlocksId.join();
-
-	var blockIdToShow;
-	if (URITradingStatus) blockIdToShow = '#assest-progress-'+URITradingStatus;
-
-	var matched = progressStopsBlocksIdString.match(new RegExp(blockIdToShow+','), 'i');
-	if (!matched) {
-		blockIdToShow = progressStopsBlocksId[0];
-	}
-
-
-	progressStopsBlocksIdString = progressStopsBlocksIdString.slice(0, -2);
-	$(progressStopsBlocksIdString).each(function () {
+	$(progressStopsBlocksId.join()).each(function () {
 		// index is NOT reliable, in case some dom is missing, or the id incorrect
 		if ('#'+this.id === blockIdToShow) {
-			var $abstractBlock = $('.record-detail-asset-abstract');
+			var $abstractBlock = $('.fund-record-detail-asset-abstract');
 
 			$abstractBlock.find('.left .value.product-caption').text(URIProductCaption);
 			$abstractBlock.find('.left .value.product-id'     ).text(URIP.productId);
-			$abstractBlock.find('.right .value.trading-type').text(URIP.action);
+			$abstractBlock.find('.right .value.trading-type'  ).text(URIP.action);
 
 			$(this).show();
 		} else {
