2c0a66b0fb449b232df1d97515dbb69eb36732cb
wulechuan <wulechuan@live.com>
2016-08-26 18:30:25 +0800

    duplicated fake records

--- app-dev/scripts/pages/my-funds-po-trading-records-list.js
+++ app-dev/scripts/pages/my-funds-po-trading-records-list.js
@@ -36,8 +36,10 @@ $(function () {
 
 	var $records = $('.fund-trading-record');
 	var possibleStatusCount = possibleStatuses.length;
+	var recordRoot, expansionContainer;
+
 	for (i = 0; i < $records.length; i++) {
-		var recordRoot = $records[i];
+		recordRoot = $records[i];
 		recordStatus = possibleStatuses[Math.floor(Math.random() * possibleStatusCount)];
 		row = progressStopsRows[recordStatus].cloneNode(true);
 		row.removeAttribute('id');
@@ -47,29 +49,71 @@ $(function () {
 			tabular.removeAttribute('id');
 		}
 
-		var container = $(recordRoot.parentNode).find('.details-container')[0];
-		if (!container) continue;
+		expansionContainer = $(recordRoot).find('.details-container')[0];
+		if (!expansionContainer) continue;
 
-		$(container).hide();
+		if (typeof recordRoot.elements !== 'object') recordRoot.elements = {};
+		if (typeof recordRoot.status !== 'object') recordRoot.status = {};
+		recordRoot.elements.abstract = $(recordRoot).find('.f-block-body.abstract')[0];
+		recordRoot.elements.expansionContainer = expansionContainer;
+		recordRoot.status.expansionContainerIsCollapsed = true;
+
+		$(expansionContainer).hide();
 		$(row).show();
 		$(tabular).show();
 
-		var rowContainer = $(container).find('.f-block-body')[0];
-		var tabularContainer = container;
+		var rowContainer = $(expansionContainer).find('.f-block-body')[0];
+		var tabularContainer = expansionContainer;
 
 		rowContainer.appendChild(row);
 		if (tabular) tabularContainer.appendChild(tabular);
+	}
 
-		$(recordRoot).on('click', (function () {
-			var $container = $(this);
-			C.l($container);
-			$container.slideToggle(function () {
-				C.l($container.outerHeight(), $container.offset());
-			});
-		}).bind(container));
+	for (i = 0; i < $records.length; i++) {
+		recordRoot = $records[i];
+		expansionContainer = recordRoot.elements.expansionContainer;
 
-		// var innerHTML = row.innerHTML;
-		// if (tabular) innerHTML += container.innerHTML;
-		// container.innerHTML = innerHTML;
+		var abstract = recordRoot.elements.abstract;
+
+		$(abstract).on('click', (function (expansionContainer) {
+			var $container = $(expansionContainer);
+			var recordRoot = this;
+			var wasCollapsed = recordRoot.status.expansionContainerIsCollapsed;
+
+			if (wasCollapsed) {
+				for (var j = 0; j < $records.length; j++) {
+					var _r = $records[j];
+					if (_r === recordRoot) continue;
+					$(_r.elements.expansionContainer).slideUp(300);
+				}
+
+				$container.slideDown(500, function () {
+					var headerH = 48;
+					var margin = 20;
+
+					var winT = window.scrollY;
+					var winH = window.innerHeight;
+					var H = $container.outerHeight();
+					var T = $container.offset().top;
+					var winTMin = Math.max(0, T + H - winH + margin);
+					var winTMax = T - headerH + margin;
+					var winNewT = Math.min(winTMax, Math.max(winTMin, winT));
+					C.l(
+						'current top:', winT,
+						'\tnew top:', winNewT,
+						'(', winTMin, ',', winTMax, ')',
+						'\twin innerHeight:', winH,
+						'\tcontainer:', T, H
+					);
+					if (winNewT !== winT) {
+						window.scrollTo(null, winNewT);
+					}
+				});
+			} else {
+				$container.slideUp();
+			}
+
+			recordRoot.status.expansionContainerIsCollapsed = !wasCollapsed;
+		}).bind(recordRoot, expansionContainer));
 	}
 });
