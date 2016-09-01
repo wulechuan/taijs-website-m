0bdf0afdc6807dd9d279715fa7bf57b4197bade3
wulechuan <wulechuan@live.com>
2016-09-01 13:14:43 +0800

    new theme colors

--- app-dev/scripts/pages/fund-entity-detail.js
+++ app-dev/scripts/pages/fund-entity-detail.js
@@ -173,7 +173,7 @@ $(function () {
 			corssHair: '#ff6600',
 			axesLabels: '#cccccc',
 			axesLines: '#f1f1f1',
-			graph: '#ffb146'
+			graph: '#c5884d'
 		};
 
 		var axesLabelsFont = {
@@ -331,7 +331,7 @@ $(function () {
 			corssHair: '#ff6600',
 			axesLabels: '#cccccc',
 			axesLines: '#f1f1f1',
-			graph1: '#ffb146',
+			graph1: '#c5884d',
 			graph2: '#ff6600',
 		};
 
@@ -506,7 +506,7 @@ $(function () {
 			corssHair: '#ff6600',
 			axesLabels: '#cccccc',
 			axesLines: '#f1f1f1',
-			graph: '#ffb146'
+			graph: '#c5884d'
 		};
 
 		var axesLabelsFont = {
@@ -663,7 +663,7 @@ $(function () {
 			corssHair: '#ff6600',
 			axesLabels: '#cccccc',
 			axesLines: '#f1f1f1',
-			graph: '#ffb146'
+			graph: '#c5884d'
 		};
 
 		var axesLabelsFont = {
