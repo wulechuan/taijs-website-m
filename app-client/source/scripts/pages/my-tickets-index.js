82d9277730f85d62df2707a082f9a8ff7edc47b5
wulechuan <wulechuan@live.com>
2016-10-26 15:13:38 +0800

    WIP: tickets list page
    panels frame ok

--- app-client/source/scripts/pages/my-tickets-index.js
+++ app-client/source/scripts/pages/my-tickets-index.js
@@ -15,10 +15,23 @@ $(function () {
 		}
 
 
-		var ticketsTabsSwiper = new window.Swiper('.swiper-container', {
+		var ticketsTabsSwiper = new window.Swiper('.swiper-container.tab-list-container', {
 			slidesPerView: 'auto',
-			centeredSlides: true,
+			freeMode: true,
+			freeModeSticky: true,
+			spaceBetween: 0,
 		});
+
+		var ticketsPanelsSwiper = new window.Swiper('.swiper-container.panels-container', {
+			slidesPerView: 'auto',
+			freeMode: false,
+		});
+
+		// ticketsTabsSwiper  .params.control = ticketsPanelsSwiper;
+		// ticketsPanelsSwiper.params.control = ticketsTabsSwiper;
+
+
+
 		// if (ticketsTabsSwiper) {
 		// 	var $pageFooter = $page.find('.page-footer');
 
@@ -45,98 +58,98 @@ $(function () {
 
 
 
-		var tabPanelSetTabList = tabPanelSet.elements.tabList;
-		var $tabPanelSetTabList = $(tabPanelSetTabList);
-		var tabPanelSetTabListContainer = tabPanelSetTabList.parentNode;
+		// var tabPanelSetTabList = tabPanelSet.elements.tabList;
+		// var $tabPanelSetTabList = $(tabPanelSetTabList);
+		// var tabPanelSetTabListContainer = tabPanelSetTabList.parentNode;
 
-		var heightOfPageHeader = $page.find('.page-header').outerHeight();
-		var heightAboveTabPanelSet = $('.content-above-tab-panel-set').outerHeight();
-		var heightOfTabPanelSetTabList = $tabPanelSetTabList.outerHeight();
-			tabPanelSetTabListContainer.style.height = heightOfTabPanelSetTabList + 'px';
+		// var heightOfPageHeader = $page.find('.page-header').outerHeight();
+		// var heightAboveTabPanelSet = $('.content-above-tab-panel-set').outerHeight();
+		// var heightOfTabPanelSetTabList = $tabPanelSetTabList.outerHeight();
+		// 	tabPanelSetTabListContainer.style.height = heightOfTabPanelSetTabList + 'px';
 
-		setupScrollingTriggerForTabPanelSetTabList(
-			heightOfPageHeader,
-			heightAboveTabPanelSet,
-			switchToFixedMode,
-			switchToFreeMode
-		);
+		// setupScrollingTriggerForTabPanelSetTabList(
+		// 	heightOfPageHeader,
+		// 	heightAboveTabPanelSet,
+		// 	switchToFixedMode,
+		// 	switchToFreeMode
+		// );
 
-		function switchToFixedMode() {
-			$tabPanelSetTabList.addClass('fixed');
-			tabPanelSetTabList.style.top = heightOfPageHeader + 'px';
-		}
+		// function switchToFixedMode() {
+		// 	$tabPanelSetTabList.addClass('fixed');
+		// 	tabPanelSetTabList.style.top = heightOfPageHeader + 'px';
+		// }
 
-		function switchToFreeMode() {
-			$tabPanelSetTabList.removeClass('fixed');
-			tabPanelSetTabList.style.top = '';
-		}
+		// function switchToFreeMode() {
+		// 	$tabPanelSetTabList.removeClass('fixed');
+		// 	tabPanelSetTabList.style.top = '';
+		// }
 
-		function setupScrollingTriggerForTabPanelSetTabList(fixedY, aboveHeight, switchToFixedMode, switchToFreeMode) {
-			function getScrollingOffsetFor(content) {
-				if (!content || content === document) {
-					return {
-						top:  -window.scrollY,
-						left: -window.scrollX
-					};
-				}
+		// function setupScrollingTriggerForTabPanelSetTabList(fixedY, aboveHeight, switchToFixedMode, switchToFreeMode) {
+		// 	function getScrollingOffsetFor(content) {
+		// 		if (!content || content === document) {
+		// 			return {
+		// 				top:  -window.scrollY,
+		// 				left: -window.scrollX
+		// 			};
+		// 		}
 
-				return $(content).offset();
-			}
-			function actionsOnScroll() {
-				var shouldSwitch = false;
-				var currentTopOffset = getScrollingOffsetFor(scrollingElement).top; // a negative value
-				currentTopOffset = -currentTopOffset;  // a positive value
-				// C.l(
-				// 	'already fixed', positionIsFixed, 
-				// 	'\t currentTopOffset', currentTopOffset, 
-				// 	'\t expasion', scrollYOffsetThresholdForExpansion,
-				// 	'\t collapse', scrollYOffsetThresholdForCollapse
-				// );
+		// 		return $(content).offset();
+		// 	}
+		// 	function actionsOnScroll() {
+		// 		var shouldSwitch = false;
+		// 		var currentTopOffset = getScrollingOffsetFor(scrollingElement).top; // a negative value
+		// 		currentTopOffset = -currentTopOffset;  // a positive value
+		// 		// C.l(
+		// 		// 	'already fixed', positionIsFixed, 
+		// 		// 	'\t currentTopOffset', currentTopOffset, 
+		// 		// 	'\t expasion', scrollYOffsetThresholdForExpansion,
+		// 		// 	'\t collapse', scrollYOffsetThresholdForCollapse
+		// 		// );
 
-				if (positionIsFixed) {
-					shouldSwitch = currentTopOffset <= scrollYOffsetThresholdForExpansion;
-					if (shouldSwitch) {
-						switchToFreeMode();
-						positionIsFixed = false;
-					}
-				} else {
-					shouldSwitch = currentTopOffset >= scrollYOffsetThresholdForCollapse;
-					if (shouldSwitch) {
-						switchToFixedMode();
-						$(scrollingElement).scrollTop(currentTopOffset);
-						positionIsFixed = true;
-					}
-				}
-			}
+		// 		if (positionIsFixed) {
+		// 			shouldSwitch = currentTopOffset <= scrollYOffsetThresholdForExpansion;
+		// 			if (shouldSwitch) {
+		// 				switchToFreeMode();
+		// 				positionIsFixed = false;
+		// 			}
+		// 		} else {
+		// 			shouldSwitch = currentTopOffset >= scrollYOffsetThresholdForCollapse;
+		// 			if (shouldSwitch) {
+		// 				switchToFixedMode();
+		// 				$(scrollingElement).scrollTop(currentTopOffset);
+		// 				positionIsFixed = true;
+		// 			}
+		// 		}
+		// 	}
 
-			var scrollingElement = document;
+		// 	var scrollingElement = document;
 
-			var triggerRatioForCallapse = 1;
-			var triggerRatioForExpansion = 1;
+		// 	var triggerRatioForCallapse = 1;
+		// 	var triggerRatioForExpansion = 1;
 
-			var pageOrPageBodyTopSpace = 48; // known
-			var aboveHeightInTotal = heightAboveTabPanelSet + pageOrPageBodyTopSpace;
+		// 	var pageOrPageBodyTopSpace = 48; // known
+		// 	var aboveHeightInTotal = heightAboveTabPanelSet + pageOrPageBodyTopSpace;
 
-			if (fixedY >= aboveHeightInTotal) {
-				return false;
-			}
+		// 	if (fixedY >= aboveHeightInTotal) {
+		// 		return false;
+		// 	}
 
-			var freeModeTargetScrollY = aboveHeightInTotal - fixedY; // a positive value
+		// 	var freeModeTargetScrollY = aboveHeightInTotal - fixedY; // a positive value
 
-			var scrollYOffsetThresholdForCollapse  = Math.abs(freeModeTargetScrollY) * Math.abs(triggerRatioForCallapse);
-			var scrollYOffsetThresholdForExpansion = Math.abs(freeModeTargetScrollY) * Math.abs(triggerRatioForExpansion);
+		// 	var scrollYOffsetThresholdForCollapse  = Math.abs(freeModeTargetScrollY) * Math.abs(triggerRatioForCallapse);
+		// 	var scrollYOffsetThresholdForExpansion = Math.abs(freeModeTargetScrollY) * Math.abs(triggerRatioForExpansion);
 
-			// C.l(
-			// 	'\n freeModeTargetScrollY', freeModeTargetScrollY,
-			// 	'\n aboveHeight', aboveHeight,
-			// 	'\n aboveHeightInTotal', aboveHeightInTotal,
-			// 	'\n scrollYOffsetThresholdForCollapse', scrollYOffsetThresholdForCollapse,
-			// 	'\n scrollYOffsetThresholdForExpansion', scrollYOffsetThresholdForExpansion
-			// );
+		// 	// C.l(
+		// 	// 	'\n freeModeTargetScrollY', freeModeTargetScrollY,
+		// 	// 	'\n aboveHeight', aboveHeight,
+		// 	// 	'\n aboveHeightInTotal', aboveHeightInTotal,
+		// 	// 	'\n scrollYOffsetThresholdForCollapse', scrollYOffsetThresholdForCollapse,
+		// 	// 	'\n scrollYOffsetThresholdForExpansion', scrollYOffsetThresholdForExpansion
+		// 	// );
 
-			var positionIsFixed = false;
-			actionsOnScroll();
-			$(scrollingElement).on('scroll', actionsOnScroll);
-		}
+		// 	var positionIsFixed = false;
+		// 	actionsOnScroll();
+		// 	$(scrollingElement).on('scroll', actionsOnScroll);
+		// }
 	})();
 });
