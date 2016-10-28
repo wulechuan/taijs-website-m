0d4b350f5e2982c30f95537872414d8d38ad9c61
wulechuan <wulechuan@live.com>
2016-10-28 18:08:21 +0800

    tickets tab-panels logic ok

--- app-client/source/scripts/pages/my-tickets-index.js
+++ app-client/source/scripts/pages/my-tickets-index.js
@@ -6,30 +6,47 @@ $(function () {
 
 
 	(function _setupTabPanelSet() {
-		// var tabPanelSet = new wlc.UI.TabPanelSet($page.find('.tab-panel-set')[0], {
-		// 	doNotShowPanelAtInit: true,
-		// 	// initTab: app.data.URIParameters.tabLabel
-		// });
-		// if (tabPanelSet.hasBeenDestroied) {
-		// 	return;
-		// }
-
-
-		var ticketsTabsSwiper = new window.Swiper('.swiper-container.tab-list-container', {
-			slidesPerView: 'auto',
-			freeMode: true,
-			// freeModeSticky: true,
-			spaceBetween: 0,
+		var tabPanelSet = new wlc.UI.TabPanelSet($page.find('#chief-panel-set')[0], {
+			// doNotShowPanelAtInit: true,
+			// initTab: app.data.URIParameters.tabLabel
 		});
+		if (tabPanelSet.hasBeenDestroied) {
+			return;
+		}
 
-		var ticketsPanelsSwiper = new window.Swiper('.swiper-container.panels-container', {
-			slidesPerView: 'auto',
-			freeMode: false,
-		});
+		var ticketsTabsSwiperRootElement   = $('#chief-panel-set .swiper-container.tab-list-container')[0];
+		var ticketsPanelsSwiperRootElement = $('#chief-panel-set .swiper-container.panel-group-container')[0];
+		var ticketsTabsSwiper, ticketsPanelsSwiper;
 
-		// ticketsTabsSwiper  .params.control = ticketsPanelsSwiper;
-		// ticketsPanelsSwiper.params.control = ticketsTabsSwiper;
+		if (ticketsTabsSwiperRootElement) {
+			$(ticketsTabsSwiperRootElement).find('> .tab-list > li').each(function (i, tab) {
+				tab.dataset.tabIndex = i;
+				$(tab).on('click', function (event) {
+					ticketsTabsSwiper.slideTo(i);
+					ticketsPanelsSwiper.slideTo(i);
+				});
+			});
 
+			ticketsTabsSwiper = new window.Swiper(ticketsTabsSwiperRootElement, {
+				slidesPerView: 'auto',
+			});
+		}
+
+		if (ticketsPanelsSwiperRootElement) {
+			$(ticketsPanelsSwiperRootElement).find('> .panel-group > .panel').each(function (i, tab) {
+				tab.dataset.panelIndex = i;
+			});
+
+			ticketsPanelsSwiper = new window.Swiper(ticketsPanelsSwiperRootElement, {
+				freeMode: false,
+				onSlideChangeStart: function (swiper) {
+					var panelToShow = swiper.slides[swiper.activeIndex];
+					var indexOfPanelToShow = panelToShow.dataset.panelIndex;
+					ticketsTabsSwiper.slideTo(indexOfPanelToShow);
+					tabPanelSet.showPanel(indexOfPanelToShow);
+				}
+			});
+		}
 
 		// tabPanelSet.showPanelViaTab(app.data.URIParameters.tabLabel || 0);
 	})();
