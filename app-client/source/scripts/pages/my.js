0b617157f175a459fc75a4b1eab108be8e8c61b4
wulechuan <wulechuan@live.com>
2016-11-01 11:57:57 +0800

    18888hongbao ok

---
+++ app-client/source/scripts/pages/my.js
@@ -0,0 +1,23 @@
+$(function () { // fake logics
+	var app = window.taijs.app;
+	var URIParameters = app.data.URIParameters;
+	var wlc = window.webLogicControls;
+	var UI = wlc.UI;
+
+
+	var $pl1 = $('#pl-get-ti-yan-jin-prompt');
+
+
+	var pl1 = $pl1[0];
+
+    if (URIParameters.firstTimeLogin) {
+		UI.popupLayersManager.show(pl1);
+    }
+
+	// $pl1.on('click', function (event) {
+	// 	var el = event.target;
+	// 	if (el === pl1 || $(el).hasClass('button-x')) {
+	// 		UI.popupLayersManager.hide(pl1);
+	// 	}
+	// });
+});
