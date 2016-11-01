25e0bbc566659eb68bc589d8e336c48bbe02bdab
wulechuan <wulechuan@live.com>
2016-11-01 18:55:06 +0800

    experience confirm masks ok

--- app-client/source/scripts/pages/my.js
+++ app-client/source/scripts/pages/my.js
@@ -5,14 +5,20 @@ $(function () { // fake logics
 	var UI = wlc.UI;
 
 
-	var $pl1 = $('#pl-get-ti-yan-jin-prompt');
+	var $pl1 = $('#pl-ti-yan-jin-availability-prompt');
+	var $pl2 = $('#pl-ti-yan-jin-proceeded-prompt');
 
 
 	var pl1 = $pl1[0];
+	var pl2 = $pl2[0];
 
-    if (URIParameters.firstTimeLogin) {
+    if (URIParameters.experienceProceeded) {
+		UI.popupLayersManager.show(pl2);
+    } else
+	// if (URIParameters.firstTimeLogin)
+	{
 		UI.popupLayersManager.show(pl1);
-    }
+	}
 
 	// $pl1.on('click', function (event) {
 	// 	// var el = event.target;
