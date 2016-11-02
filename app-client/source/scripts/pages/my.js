1cca1fea3ff7de1c7b1f3f33ee89baaca46fc4fd
wulechuan <wulechuan@live.com>
2016-11-02 17:34:56 +0800

    fixed scrolling issue

--- app-client/source/scripts/pages/my.js
+++ app-client/source/scripts/pages/my.js
@@ -4,6 +4,16 @@ $(function () { // fake logics
 	var wlc = window.webLogicControls;
 	var UI = wlc.UI;
 
+	function onShowingAnyPopupMask() {
+		setTimeout(function () {
+			window.scrollTo(0, 0);
+		}, 1);
+		document.body.style.overflow = 'hidden';
+	}
+	function onHidingAllPopupMask() {
+		document.body.style.overflow = '';
+	}
+
 
 	var $pl1 = $('#pl-ti-yan-jin-availability-prompt');
 	var $pl2 = $('#pl-ti-yan-jin-proceeded-prompt');
@@ -15,11 +25,14 @@ $(function () { // fake logics
 	var pl3 = $pl3[0];
 
     if (URIParameters.experienceBenifitAvailable) {
+		onShowingAnyPopupMask();
 		UI.popupLayersManager.show(pl3);
     } else if (URIParameters.experienceProceeded) {
+		onShowingAnyPopupMask();
 		UI.popupLayersManager.show(pl2);
     } else // if (URIParameters.firstTimeLogin)
 	{
+		onShowingAnyPopupMask();
 		UI.popupLayersManager.show(pl1);
 	}
 
@@ -34,6 +47,7 @@ $(function () { // fake logics
 		var el = event.target;
 		if ((el.getAttribute('button-action') === 'confirm')) {
 			UI.popupLayersManager.hide(pl3);
+			onHidingAllPopupMask();
 		}
 	});
 });
