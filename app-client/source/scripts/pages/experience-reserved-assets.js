b8fc0ddc075eadeaee31d0fde75ff799f965623b
wulechuan <wulechuan@live.com>
2016-11-02 16:15:48 +0800

    experience benifit tip ok

--- app-client/source/scripts/pages/experience-reserved-assets.js
+++ app-client/source/scripts/pages/experience-reserved-assets.js
@@ -2,7 +2,7 @@ $(function () {
 	var wlc = window.webLogicControls;
 	var UI = wlc.UI;
 
-	var $pl1 = $('#pl-experience-guide-step-2');
+	var $pl1 = $('#pl-experience-guide-step-1');
 	var pl1 = $pl1[0];
     UI.popupLayersManager.show(pl1);
 });
--- app-client/source/scripts/pages/my.js
+++ app-client/source/scripts/pages/my.js
@@ -7,12 +7,16 @@ $(function () { // fake logics
 
 	var $pl1 = $('#pl-ti-yan-jin-availability-prompt');
 	var $pl2 = $('#pl-ti-yan-jin-proceeded-prompt');
+	var $pl3 = $('#pl-experience-benifit-available');
 
 
 	var pl1 = $pl1[0];
 	var pl2 = $pl2[0];
+	var pl3 = $pl3[0];
 
-    if (URIParameters.experienceProceeded) {
+    if (URIParameters.experienceBenifitAvailable) {
+		UI.popupLayersManager.show(pl3);
+    } else if (URIParameters.experienceProceeded) {
 		UI.popupLayersManager.show(pl2);
     } else // if (URIParameters.firstTimeLogin)
 	{
@@ -25,4 +29,11 @@ $(function () { // fake logics
 	// 		UI.popupLayersManager.hide(pl1);
 	// 	// }
 	// });
+
+	$pl3.on('click', function (event) {
+		var el = event.target;
+		if ((el.getAttribute('button-action') === 'confirm')) {
+			UI.popupLayersManager.hide(pl3);
+		}
+	});
 });
