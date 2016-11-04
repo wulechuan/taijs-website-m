4a5c9ce2c5f9af0b09cf7c737818b0bc0158e770
wulechuan <wulechuan@live.com>
2016-11-04 15:53:57 +0800

    taijinbao animation ok

--- app-client/source/scripts/pages/my.js
+++ app-client/source/scripts/pages/my.js
@@ -50,4 +50,106 @@ $(function () { // fake logics
 			onHidingAllPopupMask();
 		}
 	});
+
+
+
+
+
+	var $anchorExperience = $('#experience-trading-record-detail-anchor');
+	var $anchorTaiJinBao = $('#tai-jin-bao-anchor');
+	var buttonForExperienceTrans = $anchorExperience.find('.interaction-hot-area')[0];
+
+	function transferingMoneyFromExperienceToTaiJinBao() {
+		// ajax: transfering money from experience to tai-jin-bao
+		onTransferingAjaxDone();
+	}
+	function onTransferingAjaxDone(result) {
+		var alreadyTransferred = false;
+		if (alreadyTransferred) return true;
+
+		var $experienceBalance = $anchorExperience.find('.right .value');
+		var $taiJinBaoBalance = $anchorTaiJinBao.find('.right .value');
+
+		var experienceBalanceValue = $experienceBalance.text().replace(/\,/g, '');
+		experienceBalanceValue = parseFloat(experienceBalanceValue) || 0;
+
+		var taiJinBaoBalanceValue = $taiJinBaoBalance.text().replace(/\,/g, '');
+		taiJinBaoBalanceValue = parseFloat(taiJinBaoBalanceValue) || 0;
+
+		taiJinBaoBalanceValue += experienceBalanceValue;
+
+		$experienceBalance.text('0');
+		$taiJinBaoBalance.text(taiJinBaoBalanceValue);
+
+		$(buttonForExperienceTrans).hide();
+	}
+
+
+
+
+
+	(function _SetupIconAnimations() {
+		function buttonForExperienceTransOnClick(event) {
+			playABunchOfCoinsAnimationOnce();
+			transferingMoneyFromExperienceToTaiJinBao();
+		}
+
+		var leadIconSymbolCoin = $anchorExperience.find('.lead-icon .icon.js-the-symbol')[0];
+		var leadIconSymbolMoneyBag = $anchorTaiJinBao.find('.lead-icon')[0];
+
+		var theCoinOfPulse = $('.coin-of-pulse')[0];
+		var aBunchOfCoinsContainer = $('.a-bunch-of-coins')[0];
+		var allCoinsOfTheBunch = $(aBunchOfCoinsContainer).find('.coin');
+
+		$anchorExperience.on('click', function (event) {
+			var clickedElementWasButton = event.target === buttonForExperienceTrans;
+			if (clickedElementWasButton) {
+				event.preventDefault();
+				event.stopPropagation();
+				buttonForExperienceTransOnClick();
+			}
+		});
+
+		function playABunchOfCoinsAnimationOnce() {
+			theCoinOfPulse.style.display = 'none';
+			aBunchOfCoinsContainer.style.display = 'block';
+			leadIconSymbolCoin.style.transform = 'scale(0.01, 0.01)';
+
+			var maxTotalDuration = 0;
+			var minTotalDuration = 1000;
+			for (var i=0; i<allCoinsOfTheBunch.length; i++) {
+				var coin = allCoinsOfTheBunch[i];
+				var aDuration = Math.random() * 0.15 + 0.7;
+				var aDelay = (Math.random() - 0.5) * 0.9 + 0.9;
+				var aTotalDuration = aDuration+aDelay;
+				if (aTotalDuration > maxTotalDuration) maxTotalDuration = aTotalDuration;
+				if (aTotalDuration < minTotalDuration) minTotalDuration = aTotalDuration;
+
+				coin.style.animation = [
+					'coin-in-a-bunch-drops-once',
+					aDuration+'s',
+					'ease-in',
+					aDelay+'s',
+					'both'
+				].join(' ');
+			}
+
+			leadIconSymbolMoneyBag.style.animationDelay = (minTotalDuration - 0.1)+'s';
+			$(leadIconSymbolMoneyBag).addClass('animating');
+
+			setTimeout(function () {
+				theCoinOfPulse.style.display = '';
+				aBunchOfCoinsContainer.style.display = '';
+				leadIconSymbolCoin.style.transform = '';
+				// leadIconSymbolMoneyBag.style.animation = '';
+				leadIconSymbolMoneyBag.style.animationDelay = '';
+				$(leadIconSymbolMoneyBag).removeClass('animating');
+
+				for (var i=0; i<allCoinsOfTheBunch.length; i++) {
+					var coin = allCoinsOfTheBunch[i];
+					coin.style.animation = ' ';
+				}
+			}, maxTotalDuration*1111);
+		}
+	})();
 });
